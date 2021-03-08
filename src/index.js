f26bc4830be84fb1ed402cb2cff8455df5d1e200
Facundo <facucristaldorodriguez@gmail.com>
2021-03-08 09:02:50 -0300

    styled components

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StyledComponents from './components/StyledComponents';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StyledComponents/>
   </React.StrictMode>,
   document.getElementById('root')
 );
